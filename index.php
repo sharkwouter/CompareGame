3b7666b475a1230a4b61e39461285e3bfc33981b
Wouter Wijsman <wwijsman@live.nl>
2017-10-23 15:54:40 +0200

    Fixed up form on index You can now no longer update, but that will be added to update.php later

--- index.php
+++ index.php
@@ -5,44 +5,43 @@ To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
 <?php
-    //Include classes
-    include_once 'classes/Url.php';
-    include_once 'classes/Game.php';
-    include_once 'classes/Database.php';
-    include_once 'classes/Parser.php';
-    include_once 'classes/Import.php';
-    include_once 'classes/Navbar.php';
-    
-    //Load config
-    $configFile = "config.php";
-    if(is_file($configFile)) {
-        include_once $configFile;
-    }
-    
-    //Create database object
-    $db = new Database($dbname, $dbip, $dbport, $dbuser, $dbpass);
-    
-    //Update database if asked
-    $updateButton = filter_input(INPUT_POST, "update");
-    if(isset($updateButton)) {
-        //Create import object, will trigger parsing every source
-        $import = new Import();
-        //Add all found games to the database
-        $games = $import->getGamesList();
-        foreach ($games as $game){
-            $db->addGame($game);
-        }
-    }
-    
-    //Get what has been searched, but only if the search button has been pressed
+//Include classes
+include_once 'classes/Url.php';
+include_once 'classes/Game.php';
+include_once 'classes/Database.php';
+include_once 'classes/Parser.php';
+include_once 'classes/Import.php';
+include_once 'classes/Navbar.php';
+
+//Load config
+$configFile = "config.php";
+if (is_file($configFile)) {
+    include_once $configFile;
+}
+
+//Create database object
+$db = new Database($dbname, $dbip, $dbport, $dbuser, $dbpass);
+
+//Update database if asked
+//    $updateButton = filter_input(INPUT_POST, "update");
+//    if(isset($updateButton)) {
+//        //Create import object, will trigger parsing every source
+//        $import = new Import();
+//        //Add all found games to the database
+//        $games = $import->getGamesList();
+//        foreach ($games as $game){
+//            $db->addGame($game);
+//        }
+//    }
+
+//Get what has been searched, but only if the search button has been pressed
+$searchString = filter_input(INPUT_GET, "search");
+if(empty($searchString)){
     $searchString = "";
-    $submitButton = filter_input(INPUT_POST, "submit");
-    if(isset($submitButton)) {
-        $searchString = filter_input(INPUT_POST, "search");
-    }
-    
-    //Create navbar object
-    $navbar = new Navbar();
+}
+
+//Create navbar object
+$navbar = new Navbar();
 ?>
 <html>
     <head>
@@ -50,14 +49,14 @@ and open the template in the editor.
         <title></title>
     </head>
     <body>
-        <?=$navbar->printNavbar()?>
-        <form method="post">
-            Search: 
-            <input type="text" name="search" value="<?=$searchString?>" />
-            <input type="submit" name="submit" value="Search" />
-            <input type="submit" value="Clear Search" />
-            <input type="submit" name="update" value="Update Database" />
-        </form>
+        <?= $navbar->printNavbar() ?>
+        <div>
+            <form method="get">
+                Search: 
+                <input type="text" name="search" value="<?= $searchString ?>" />
+                <input type="submit" value="Search" />
+            </form>
+        </div>
         <?php
         //Show a set of games depending on if the search was used
         $db->searchGames($searchString);
