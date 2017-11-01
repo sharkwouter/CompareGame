452077241f2f34754d56e29d184e073e8fb806af
Wouter Wijsman <wwijsman@live.nl>
2017-11-01 22:33:57 +0100

    Some code cleanup in import.php

--- classes/Import.php
+++ classes/Import.php
@@ -5,37 +5,38 @@ include_once 'classes/Database.php';
 
 //This class makes the import happen for all stores
 class Import {
-    private $gameList = array();
-    
     //Database object from the constructor
     private $db;
-    
+
     public function __construct(Database $db) {
         //Get access to the database
         $this->db = $db;
     }
-    
+
     //This updates the games in the database for the current store and platform
-    public function update(int $storeid, int $platformid){
+    public function update(int $storeid, int $platformid) {
         //Get the data to give to the parser from the database
-        $data = $this->db->getParseData($storeid,$platformid);
+        $data = $this->db->getParseData($storeid, $platformid);
+
         //Parse the page
-        $parser = new Parser($data["company"], $data["platform"], new URL($data["url"]), $data["product"], $data["name"], $data["price"], $data["link"], $data["nextpage"]);
-        
-        //Create our gameslist
-        $this->gameList = $parser->getGamesList();
-        
-        //Add the games to the database
-        $this->updateDatabase();
-        
-        //Notify user that we tried something, there is no error check yet
-        print("<div class='message'><b>The database has been updated</b></div>");
+        if (!empty($data)) {
+            $parser = new Parser($data["company"], $data["platform"], new URL($data["url"]), $data["product"], $data["name"], $data["price"], $data["link"], $data["nextpage"]);
+
+            //Add the games to the database
+            $this->addGames($parser->getGamesList());
+
+            //Notify user that we tried something, there is no error check yet
+            print("<div class='message'><b>The database has been updated</b></div>");
+        } else {
+            print("<div class='message'><b>The requested website/store combination doesn't exist</b></div>");
+        }
     }
-    
+
     //Add all games in the gamesList to the database
-    private function updateDatabase() {
-        foreach($this->gameList as $game){
+    private function addGames($gameList) {
+        foreach ($this->gameList as $game) {
             $this->db->addGame($game);
         }
     }
+
 }
