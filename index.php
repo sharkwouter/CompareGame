77353bc0741b6303dfaf737827e8c00f0348a59e
Wouter Wijsman <wwijsman@live.nl>
2017-11-04 17:11:14 +0100

    Clean-up html index.php

--- index.php
+++ index.php
@@ -34,7 +34,7 @@ $navbar = new Navbar();
     </head>
     <body>
         <?= $navbar->printNavbar() ?>
-        <div>
+        <article>
             <form method="get">
                 Search: 
                 <input type="text" name="search" value="<?= $searchString ?>" />
@@ -54,41 +54,41 @@ $navbar = new Navbar();
                 </select>
                 <input type="submit" value="Search" />
             </form>
-        </div>
-        <table>
-            <?php
-            //Print table header with order buttons
-            $fields = array("name", "price", "platform", "store");
-            print("<tr>");
-            foreach ($fields as $field) {
-                //These headers include buttons which allow for sorting
-                print("<th><a href='index.php?search=" . $searchString . "&platform=" . $platform . "&orderby=" . $field . "&order=0'>↑</a> " . ucfirst($field) . " <a href='index.php?search=" . $searchString . "&platform=" . $platform . "&orderby=" . $field . "&order=1'>↓</a></th>");
-            }
-            print("</tr>\n");
-
-            //Show a set of games depending on if the search was used
-            foreach ($gameList as $game) {
-                //Get data from the game object
-                $data = $game->returnData();
-                //Print the table row
+            <table>
+                <?php
+                //Print table header with order buttons
+                $fields = array("name", "price", "platform", "store");
                 print("<tr>");
-                print("<td><a href='" . $data["link"] . "'>" . htmlspecialchars($data["name"]) . "</a></td>");
-                print("<td>&euro;" . sprintf('%01.2f', $data["price"]) . "</td>");
-                print("<td>" . htmlspecialchars($data["platform"]) . "</td>");
-                print("<td>" . htmlspecialchars($data["store"]) . "</td>");
+                foreach ($fields as $field) {
+                    //These headers include buttons which allow for sorting
+                    print("<th><a href='index.php?search=" . $searchString . "&platform=" . $platform . "&orderby=" . $field . "&order=0'>↑</a> " . ucfirst($field) . " <a href='index.php?search=" . $searchString . "&platform=" . $platform . "&orderby=" . $field . "&order=1'>↓</a></th>");
+                }
                 print("</tr>\n");
-            }
-            ?>
-        </table>
+
+                //Show a set of games depending on if the search was used
+                foreach ($gameList as $game) {
+                    //Get data from the game object
+                    $data = $game->returnData();
+                    //Print the table row
+                    print("<tr>");
+                    print("<td><a href='" . $data["link"] . "'>" . htmlspecialchars($data["name"]) . "</a></td>");
+                    print("<td>&euro;" . sprintf('%01.2f', $data["price"]) . "</td>");
+                    print("<td>" . htmlspecialchars($data["platform"]) . "</td>");
+                    print("<td>" . htmlspecialchars($data["store"]) . "</td>");
+                    print("</tr>\n");
+                }
+                ?>
+            </table>
+        </article>
         <footer>
-        <?php
-            if($page > 0){
-                print("<a href='index.php?search=" . $searchString . "&platform=" . $platform . "&orderby=" . $orderBy . "&order=".$orderDirection."&page=".($page-1)."&pagesize=".$pageSize."'>previous page</a> "); 
+            <?php
+            if ($page > 0) {
+                print("<a href='index.php?search=" . $searchString . "&platform=" . $platform . "&orderby=" . $orderBy . "&order=" . $orderDirection . "&page=" . ($page - 1) . "&pagesize=" . $pageSize . "'>previous page</a> ");
             } else {
                 print("<u>previous page</u> ");
             }
-            print("<a href='index.php?search=" . $searchString . "&platform=" . $platform . "&orderby=" . $orderBy . "&order=".$orderDirection."&page=".($page+1)."&pagesize=".$pageSize."'>next page</a> ");
-        ?>
+            print("<a href='index.php?search=" . $searchString . "&platform=" . $platform . "&orderby=" . $orderBy . "&order=" . $orderDirection . "&page=" . ($page + 1) . "&pagesize=" . $pageSize . "'>next page</a> ");
+            ?>
         </footer>
     </body>
 </html>
